dd38dfeb9a0e3d9c33b5323dc3e1806f737d056c
Nedralia <jan@nedralia.se>
2026-04-22 15:39:18 +0200

    Remove reminder Lambda function and related resources; update outputs and variables

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,19 +1,9 @@
-output "lambda_function_name" {
-  description = "Name of the deployed reminder Lambda."
-  value       = aws_lambda_function.send_reminder.function_name
-}
-
 output "schedule_bucket_name" {
   description = "S3 bucket storing the schedule CSV."
   value       = aws_s3_bucket.schedule_data.bucket
 }
 
 output "schedule_object_key" {
-  description = "S3 object key used by the reminder Lambda."
+  description = "S3 object key for the schedule CSV."
   value       = aws_s3_object.schedule_csv.key
-}
-
-output "eventbridge_rule_name" {
-  description = "Name of the EventBridge rule that triggers the Lambda."
-  value       = aws_cloudwatch_event_rule.send_reminder_daily.name
 }
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -10,12 +10,6 @@ variable "environment" {
   default     = "prod"
 }
 
-variable "lambda_function_name" {
-  description = "Name of the reminder Lambda function."
-  type        = string
-  default     = "sfk-scheduler-send-reminder"
-}
-
 variable "schedule_bucket_name_prefix" {
   description = "Prefix used to build the S3 bucket name that stores the schedule CSV."
   type        = string
@@ -26,18 +20,6 @@ variable "schedule_object_key" {
   description = "S3 object key for the schedule CSV file."
   type        = string
   default     = "schedule.csv"
-}
-
-variable "reminder_schedule_expression" {
-  description = "EventBridge schedule expression for the reminder Lambda."
-  type        = string
-  default     = "cron(0 6 * * ? *)"
-}
-
-variable "log_retention_days" {
-  description = "Retention period for Lambda logs in CloudWatch."
-  type        = number
-  default     = 14
 }
 
 variable "tags" {
