0bf949b4e044df094aeeec0005e9851f3a0820f8
Nedralia <jan@nedralia.se>
2026-05-02 19:50:11 +0200

    Add deploy script for S3 bucket deployment and CloudFront cache invalidation

---
+++ src/page/deploy.sh
@@ -0,0 +1,20 @@
+# Deploy code from /dist to AWS S3 bucket and invalidate CloudFront cache
+# Usage: ./deploy.sh <bucket-name> <cloudfront-distribution-id>
+
+#!/bin/bash
+set -e
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <bucket-name> <cloudfront-distribution-id>"
+    exit 1
+fi
+
+BUCKET_NAME=$1
+DISTRIBUTION_ID=$2
+
+# Sync the /dist directory to the S3 bucket
+aws s3 sync ./dist s3://$BUCKET_NAME --delete
+
+# Invalidate CloudFront cache
+# aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
+
+echo "Deployment complete. S3 bucket: $BUCKET_NAME, CloudFront distribution: $DISTRIBUTION_ID"
