f56a7fb5760ffe4ae37817c3a531a0ba2872beb9
Nedralia <jan@nedralia.se>
2026-06-17 14:43:37 +0200

    Refactor Terraform configuration for consistent formatting in API Gateway and Reminder Lambda modules

--- infrastructure/modules/api_gateway/main.tf
+++ infrastructure/modules/api_gateway/main.tf
@@ -8,10 +8,10 @@ resource "aws_api_gateway_rest_api" "this" {
 }
 
 resource "aws_api_gateway_integration" "lambda" {
-  rest_api_id = aws_api_gateway_rest_api.this.id
-  resource_id = aws_api_gateway_resource.this.id
-  http_method = aws_api_gateway_method.this.http_method
-  type        = "AWS_PROXY"
+  rest_api_id             = aws_api_gateway_rest_api.this.id
+  resource_id             = aws_api_gateway_resource.this.id
+  http_method             = aws_api_gateway_method.this.http_method
+  type                    = "AWS_PROXY"
   integration_http_method = "POST"
-  uri         = element(var.lambda_arns, 0)
+  uri                     = element(var.lambda_arns, 0)
 }
